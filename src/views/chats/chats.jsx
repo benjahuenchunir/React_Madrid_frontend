bba1f15d2fb88412e6ca7726f5b7e8b94af096e7
bahuenchunir <bahuenchunir@uc.cl>
2024-05-28 09:13:55 -0400

    feat(chats-display): filter chats

--- src/views/chats/chats.jsx
+++ src/views/chats/chats.jsx
@@ -4,6 +4,7 @@ import ChatCard from './components/ChatCard';
 
 const Chats = () => {
     const [users, setUsers] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         fetch('https://randomuser.me/api?results=5')
@@ -11,16 +12,30 @@ const Chats = () => {
             .then(data => setUsers(data.results));
     }, []);
 
+    const filteredUsers = users.filter(user =>
+        `${user.name.first} ${user.name.last}`.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return (
         <div>
-            {users.map((user, index) => (
-                <ChatCard 
-                    key={index}
-                    profilePic={user.picture.large} 
-                    contactName={`${user.name.first} ${user.name.last}`} 
-                    lastMessage="Hello there!" 
+            <div class="chats">
+                <input 
+                    type="text" 
+                    placeholder="Buscar en mis chats" 
+                    onChange={event => setSearchQuery(event.target.value)} 
                 />
-            ))}
+                {filteredUsers.map((user, index) => (
+                    <ChatCard 
+                        key={index}
+                        profilePic={user.picture.large} 
+                        contactName={`${user.name.first} ${user.name.last}`} 
+                        lastMessage="Hola!" 
+                    />
+                ))}
+            </div>
+            <div class="chat-details">
+                
+            </div>
         </div>
     );
 };
