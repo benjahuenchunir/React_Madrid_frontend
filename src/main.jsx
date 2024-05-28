827f8ce374abd346827020d0b9f10bf668e971f3
bahuenchunir <bahuenchunir@uc.cl>
2024-05-28 12:00:46 -0400

    feat(navigation): implement navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Chats from './views/chats/chats.jsx'
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Chats />
+    <BrowserRouter>
+     <App />
+     </BrowserRouter>
   </React.StrictMode>,
 )
