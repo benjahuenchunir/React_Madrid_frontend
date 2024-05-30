beaf43d45976471eb1fcef650e2c909ca308c14a
benjahuenchunir <bahuenchunir@gmail.com>
2024-05-30 17:29:03 -0400

    feat(navbar): reusing links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,28 +9,45 @@ function Navbar(): JSX.Element {
         setIsOpen(false);
     };
 
+    const linkGroups = [
+        [
+            { to: "/", text: "Home" },
+            { to: "/chats", text: "Chats" },
+            { to: "/about-us", text: "Nosotros" },
+        ],
+        [
+            { to: "/login", text: "Login" },
+            { to: "/register", text: "Register" },
+        ]
+    ];
+
     return (
         <>
             <nav>
-                <ul>
-                    <li><CustomNavLink to="/">Home</CustomNavLink></li>
-                    <li><CustomNavLink to="/chats">Chats</CustomNavLink></li>
-                    <li><CustomNavLink to="/about-us">Nosotros</CustomNavLink></li>
-                </ul>
-                <ul>
-                    <li><CustomNavLink to="/login">Login</CustomNavLink></li>
-                    <li><CustomNavLink to="/register">Register</CustomNavLink></li>
-                </ul>
+                {linkGroups.map((group, groupIndex) => (
+                    <ul key={groupIndex}>
+                        {group.map((link, linkIndex) => (
+                            <li key={linkIndex}>
+                                <CustomNavLink to={link.to}>{link.text}</CustomNavLink>
+                            </li>
+                        ))}
+                    </ul>
+                ))}
                 <button className="hamburger-menu" onClick={() => setIsOpen(!isOpen)}/>
             </nav>
             {
                 isOpen && (
                     <div className="dropdown-menu">
-                        <CustomNavLink to="/" onClick={closeMenu}>Home</CustomNavLink>
-                        <CustomNavLink to="/chats" onClick={closeMenu}>Chats</CustomNavLink>
-                        <CustomNavLink to="/about-us" onClick={closeMenu}>Nosotros</CustomNavLink>
-                        <CustomNavLink to="/login" onClick={closeMenu}>Login</CustomNavLink>
-                        <CustomNavLink to="/register" onClick={closeMenu}>Register</CustomNavLink>
+                        {linkGroups.map((group, groupIndex) => (
+                            <React.Fragment key={groupIndex}>
+                                {group.map((link, linkIndex) => (
+                                    <CustomNavLink key={linkIndex} to={link.to} onClick={closeMenu}>
+                                        {link.text}
+                                    </CustomNavLink>
+                                ))}
+                                {groupIndex < linkGroups.length - 1 && <div className="divider"></div>}
+                            </React.Fragment>
+                        ))}
                     </div>
                 )
             }
