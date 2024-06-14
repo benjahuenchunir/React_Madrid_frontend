eb17857e4c48bf6fa932c41fbf210fc36d413b82
fvidalf <fvidalf@uc.cl>
2024-06-14 12:17:47 -0400

    feat(chats-auth): Add auth handling for get user chats request.

--- src/views/chats/chats.jsx
+++ src/views/chats/chats.jsx
@@ -2,17 +2,41 @@ import { useEffect, useState } from 'react';
 import './chats.scss';
 import ChatCard from './components/ChatCard';
 import ChatDetails from './components/ChatDetails';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
 
 const Chats = () => {
+    const [isAuthorized, setIsAuthorized] = useState(true);
     const [chats, setChats] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
     const [selectedChat, setSelectedChat] = useState(null);
 
     useEffect(() => {
-        fetch(import.meta.env.VITE_BACKEND_URL + '/chats?userId=1') // TODO use actual user id
+        const token = localStorage.getItem('token');
+        let userId = 1;
+        if (token) {
+            const decodedToken = jwtDecode(token);
+            userId = decodedToken.sub;
+        }
+        config = {
+            method: 'get',
+            url: import.meta.env.VITE_BACKEND_URL + `/chats?userId=${userId}`,
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }
+        axios(config)
             .then(response => response.json())
-            .then(data => setChats(data));
+            .then(data => setChats(data))
+            .catch(error => {
+                console.log('error', error)
+                setIsAuthorized(false);
+            });
     }, []);
+
+    if (!isAuthorized) {
+        return <p>Unauthorized</p>;
+    }
 
     const filteredChats = chats.filter(chat =>
         `${chat.name}`.toLowerCase().includes(searchQuery.toLowerCase())
