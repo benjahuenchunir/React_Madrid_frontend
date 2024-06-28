50563828156397642662c2e4da83e2a2e00f571d
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-28 19:07:52 -0400

    feat(NewChatMenu): create group form

--- src/views/chats/components/NewChatMenu/NewChatMenu.jsx
+++ src/views/chats/components/NewChatMenu/NewChatMenu.jsx
@@ -3,16 +3,27 @@ import { useApi } from '../../api';
 import { useAuth } from '../../../../auth/useAuth';
 import "../NewChatMenu/NewChatMenu.scss";
 import PropTypes from 'prop-types';
+import { ChatModes } from '../../constants';
+
+const NewChatState = {
+    NORMAL: 'normal',
+    SELECTING_USERS: 'selecting_users',
+    CREATING_GROUP: 'creating_group'
+}
 
 const NewChatMenu = ({ onClose, buttonRef, onDMReceived, onNewDM }) => {
+    const [state, setState] = useState(false)
     const [users, setUsers] = useState([]);
     const [filter, setFilter] = useState('');
     const containerRef = useRef()
     const { token, idUser } = useAuth();
     const api = useApi(token);
-    // For creating chats
-    const [isSelectingUsers, setIsSelectingUsers] = useState(false)
+    // For creating group
     const [selectedUsers, setSelectedUsers] = useState([]);
+    const [groupName, setGroupName] = useState('');
+    const [chatMode, setChatMode] = useState(ChatModes.EVERYONE)
+    const [imagePreviewUrl, setImagePreviewUrl] = useState('');
+    const fileInputRef = useRef(null);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -60,7 +71,7 @@ const NewChatMenu = ({ onClose, buttonRef, onDMReceived, onNewDM }) => {
     );
 
     const onUserClicked = async (user) => {
-        if (isSelectingUsers) {
+        if (state === NewChatState.SELECTING_USERS) {
             handleUserSelectChange(user.id);
             return;
         }
@@ -105,43 +116,126 @@ const NewChatMenu = ({ onClose, buttonRef, onDMReceived, onNewDM }) => {
     }
 
     const onBack = () => {
-        setIsSelectingUsers(false);
-        setSelectedUsers([]);
+        if (state === NewChatState.SELECTING_USERS) {
+            setState(NewChatState.NORMAL)
+            setSelectedUsers([]);
+        } else {
+            setState(NewChatState.SELECTING_USERS)
+            setImagePreviewUrl('');
+            setGroupName('');
+            setChatMode(ChatModes.EVERYONE);
+        }
+    }
+
+    const onNextClicked = () => {
+        if (selectedUsers.length > 0) {
+            setState(NewChatState.CREATING_GROUP)
+        }
+    }
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImagePreviewUrl(reader.result);
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const onCreateClicked = async () => {
+        // try {
+        //     const newChat = {
+        //         name: 'Nuevo grupo',
+        //         users: selectedUsers.map(id => ({
+        //             id,
+        //             role: 'owner'
+        //         })),
+        //         imageUrl: null,
+        //         canSendMessage: true,
+        //         isCreatingDM: false
+        //     }
+        //     const chat = await api.post(`/chats`, newChat);
+        //     onDMReceived(chat.id);
+        //     onClose();
+        // } catch (error) {
+        //     console.error('Error:', error);
+        // }
     }
 
     return (
         <div id="new-chat-menu" ref={containerRef}>
-            {isSelectingUsers && <button id="btn-back" onClick={onBack} />}
+            {state !== NewChatState.NORMAL && <button id="btn-back" onClick={onBack} />}
             <button id="btn-close" onClick={onClose} />
-
             <div id="inner-content">
-                <div className="input-container">
-                    <input
-                        type="text"
-                        placeholder="Buscar usuario..."
-                        value={filter}
-                        onChange={handleFilterChange}
-                        className='input-filter-users'
-                    />
-                </div>
-                {!isSelectingUsers && <div onClick={() => setIsSelectingUsers(true)} className="user-container">
-                    <p>+ Crear grupo</p>
-                </div>}
-                {filteredUsers.map(user => 
-                    !(isSelectingUsers && user.id === idUser) && (
-                        <div key={user.id} onClick={() => onUserClicked(user)} className="user-container">
-                            <img src={user.profilePictureUrl} className='profile-picture' />
-                            <p>{user.id === idUser ? 'Yo' : `${user.name} ${user.lastName}`}</p>
-                            {isSelectingUsers && <input
-                                type="checkbox"
-                                checked={selectedUsers.includes(user.id)}
-                                className='user-selection-checkbox'
-                                readOnly={true}
-                            />}
-                        </div>)
-                
+                {state === NewChatState.CREATING_GROUP ? (
+                    <>
+                        <div className="image-and-name-row">
+                            {imagePreviewUrl ? (
+                                <img src={imagePreviewUrl} alt="Group preview" className="image-preview" onClick={() => fileInputRef.current.click()} />
+                            ) : (
+                                <div className="image-preview-placeholder" onClick={() => fileInputRef.current.click()} />
+                            )}
+                            <input
+                                type="file"
+                                className='input-group-image'
+                                onChange={handleImageChange}
+                                ref={fileInputRef}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Nombre del grupo"
+                                value={groupName}
+                                onChange={(e) => setGroupName(e.target.value)}
+                                className='input-group-name'
+                            />
+                        </div>
+                        <select value={chatMode} onChange={(event) => setChatMode(event.target.value)} className='report-type-selector'>
+                            {Object.entries(ChatModes).map(([key, value]) =>
+                                value !== ChatModes.DM && (
+                                    <option key={key} value={value}>
+                                        {value}
+                                    </option>)
+                            )}
+                        </select>
+                        <button className="create-button" onClick={onCreateClicked}> Crear grupo</button>
+                    </>
+                ) : (
+                    <>
+                        <div className="input-container">
+                            <input
+                                type="text"
+                                placeholder="Buscar usuario..."
+                                value={filter}
+                                onChange={handleFilterChange}
+                                className='input-filter-users'
+                            />
+                        </div>
+                        {!(state === NewChatState.SELECTING_USERS) && <div onClick={() => setState(NewChatState.SELECTING_USERS)} className="user-container">
+                            <p>+ Crear grupo</p>
+                        </div>}
+                        {filteredUsers.map(user =>
+                            !(state === NewChatState.SELECTING_USERS && user.id === idUser) && (
+                                <div key={user.id} onClick={() => onUserClicked(user)} className="user-container">
+                                    <img src={user.profilePictureUrl} className='profile-picture' />
+                                    <p>{user.id === idUser ? 'Yo' : `${user.name} ${user.lastName}`}</p>
+                                    {state === NewChatState.SELECTING_USERS && <input
+                                        type="checkbox"
+                                        checked={selectedUsers.includes(user.id)}
+                                        className='user-selection-checkbox'
+                                        readOnly={true}
+                                    />}
+                                </div>)
+
+                        )}
+                        {state === NewChatState.SELECTING_USERS && <button className="next-button" onClick={onNextClicked}> Siguiente</button>}
+                    </>
                 )}
             </div>
+
         </div>
     );
 };
