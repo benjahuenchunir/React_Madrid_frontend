97ea02cdd7b67880dec5e6d4819ee2ff12cacf15
benjahuenchunir <bahuenchunir@gmail.com>
2024-06-28 16:12:56 -0400

    fix(profile): fix white before user load

--- src/views/profile/profile.jsx
+++ src/views/profile/profile.jsx
@@ -1,43 +1,31 @@
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { jwtDecode } from 'jwt-decode'; 
 import { useAuth } from '../../auth/useAuth.js';
 import './profile.scss';
 import ProfileCard from './components/profileCard.jsx';
 
 const Profile = () => {
     const svgRef = useRef(null);
-    const { token, logout } = useAuth();
-    const [isAuthorized, setIsAuthorized] = useState(true);
+    const { token, logout, idUser } = useAuth();
     const [userProfile, setUserProfile] = useState(null);
 
     useEffect(() => {
-        let userId = null;
+        const config = {
+            method: 'get',
+            url: `${import.meta.env.VITE_BACKEND_URL}/users/${idUser}`,
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        };
 
-        if (token && token !== 'null') {
-            const decodedToken = jwtDecode(token);
-            userId = decodedToken.sub;
-
-            const config = {
-                method: 'get',
-                url: `${import.meta.env.VITE_BACKEND_URL}/users/${userId}`,
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            };
-
-            axios(config)
-                .then(response => {
-                    setUserProfile(response.data);
-                })
-                .catch(error => {
-                    console.error('Error fetching user profile:', error);
-                    setIsAuthorized(false);
-                });
-        } else {
-            setIsAuthorized(false);
-        }
-    }, [token]);
+        axios(config)
+            .then(response => {
+                setUserProfile(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching user profile:', error);
+            });
+    }, [token, idUser]);
 
     useEffect(() => {
         const adjustSvgPosition = () => {
@@ -70,14 +58,6 @@ const Profile = () => {
         };
     }, []);
 
-    if (!isAuthorized) {
-        return <p>Unauthorized</p>;
-    }
-
-    if (!userProfile) {
-        return <p>Loading...</p>;
-    }
-
     const handleDeleteAccount = () => {
         if (userProfile) {
             const config = {
@@ -91,7 +71,7 @@ const Profile = () => {
             axios(config)
                 .then(response => {
                     console.log('Account deleted:', response.data);
-                    logout(); 
+                    logout();
                 })
                 .catch(error => {
                     console.error('Error deleting account:', error);
@@ -103,12 +83,12 @@ const Profile = () => {
         <div id="profile-container">
             <div className="content">
                 <div className="profile-container">
-                    <img ref={svgRef} src="/ship_with_stars.svg" alt="Ship on ground" className="profile-fixed-bottom"/>
-                    <ProfileCard
+                    <img ref={svgRef} src="/ship_with_stars.svg" alt="Ship on ground" className="profile-fixed-bottom" />
+                    {userProfile && <ProfileCard
                         userProfile={userProfile}
                         logout={logout}
                         handleDeleteAccount={handleDeleteAccount}
-                    />
+                    />}
                 </div>
             </div>
         </div>
